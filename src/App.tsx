8f128e5e2bb583d19fd6651d63889fa91fe9418b
0qy <linqianye@tutanota.com>
2022-05-09 08:42:50 +1000

    linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import './App.css';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CssBaseline>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
+      </div>
+    </CssBaseline>
   );
 }
 
