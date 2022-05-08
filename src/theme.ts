5b80f956d2db9d69e39a1ad30f9872cfe9e84ef6
0qy <linqianye@tutanota.com>
2022-05-09 08:42:28 +1000

    init mui theme

---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#e97826",
+        },
+        secondary: {
+            main: "#7f7f7f",
+        },
+        background: {
+            default: "#d9d9d9",
+        }
+    }
+})
+
+export default theme;
