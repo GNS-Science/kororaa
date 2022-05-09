df2d3457e9f1f60746a08417c3a5f601f88fb3e2
0qy <linqianye@tutanota.com>
2022-05-09 10:05:14 +1000

    add navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { styled } from '@mui/material/styles';
+import { flexbox, height } from '@mui/system';
+import { Link, Typography } from '@mui/material';
+
+const NavBarContainer = styled('div')(({ theme }) => ({
+  backgroundColor: theme.palette.info.main,
+  height: 120,
+  borderBottom: `10px solid ${theme.palette.primary.main}`,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-around',
+}));
+
+const LinkItems = styled('div')({
+  height: '85%',
+  width: '70%',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+  alignItems: 'flex-end',
+});
+
+const NavBar: React.FC = () => {
+  return (
+    <>
+      <NavBarContainer>
+        <img src="/GNS.png" />
+        <LinkItems>
+          <Typography variant="h5">Home</Typography>
+          <Typography variant="h5">Hazard Curves</Typography>
+          <Typography variant="h5">Hazard Maps</Typography>
+          <Typography variant="h5">Rupture Sets</Typography>
+          <Typography variant="h5">Info &amp; Input Files</Typography>
+          <Typography variant="h5">Help</Typography>
+        </LinkItems>
+        <img src="/NSHM.png" />
+      </NavBarContainer>
+    </>
+  );
+};
+
+export default NavBar;
