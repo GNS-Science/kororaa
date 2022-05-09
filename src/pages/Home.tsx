81ad5f5473d4fd64b8804ae150193640ac977bfd
0qy <linqianye@tutanota.com>
2022-05-09 13:55:34 +1000

    update footer and landing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import { Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
+import CardButton from '../components/CardButton';
+
+const HomeContainer = styled('div')({
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+
 const InfoContainer = styled('div')({
   display: 'flex',
   flexDirection: 'column',
@@ -12,20 +22,30 @@ const InfoContainer = styled('div')({
 });
 
 const CardButtonsContainer = styled('div')({
+  width: '100%',
+  maxWidth: 1300,
+  height: '100vh',
+  padding: 10,
   display: 'flex',
   flexWrap: 'wrap',
   flexDirection: 'row',
+  justifyContent: 'space-around',
 });
 
 const Home: React.FC = () => {
   return (
-    <>
+    <HomeContainer>
       <InfoContainer>
         <Typography variant="h1">New Zealand National Seismic Hazard Model</Typography>
         <Typography>Welcome to the New Zealand NSHM . This is the 2022 revision to the model. Here’s a link back to the explaination and background info on the model</Typography>
       </InfoContainer>
-      <CardButtonsContainer></CardButtonsContainer>
-    </>
+      <CardButtonsContainer>
+        <CardButton title="Hazard Curves" text="Hazard Curves to view" img="/hazardCurves.png" />
+        <CardButton title="Hazard Maps" text="Hazard Maps to view" img="/hazardMaps.png" />
+        <CardButton title="Rupture Sets" text="Rupture Sets to view" img="/ruptureSets.png" />
+        <CardButton title="Model Information, Reports, and Input Files" text="More Information" img="/info.jpg" />
+      </CardButtonsContainer>
+    </HomeContainer>
   );
 };
 
