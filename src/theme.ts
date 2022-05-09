4d4cc55d244c7ced7e9c8739a93204e213aa1a4b
0qy <linqianye@tutanota.com>
2022-05-10 09:38:04 +1000

    responsive typography

--- src/theme.ts
+++ src/theme.ts
@@ -14,15 +14,61 @@ const theme = createTheme({
   },
   typography: {
     fontSize: 14,
-    h1: {
-      fontWeight: 700,
-      fontSize: '3rem',
-    },
-    h5: {
-      fontWeight: 700,
-      fontSize: '1.2rem',
+    h6: {
+      fontSize: '1rem',
     },
   },
 });
+
+theme.typography.h1 = {
+  fontWeight: 700,
+  fontSize: '2.488rem',
+  [theme.breakpoints.down('md')]: {
+    fontSize: '1.802rem',
+  },
+  [theme.breakpoints.down('sm')]: {
+    fontSize: '1.383rem',
+  },
+};
+
+theme.typography.h2 = {
+  fontSize: '2.074rem',
+  [theme.breakpoints.down('md')]: {
+    fontSize: '1.602rem',
+  },
+  [theme.breakpoints.down('sm')]: {
+    fontSize: '1.296rem',
+  },
+};
+
+theme.typography.h3 = {
+  fontSize: '1.728rem',
+  [theme.breakpoints.down('md')]: {
+    fontSize: '1.424rem',
+  },
+  [theme.breakpoints.down('sm')]: {
+    fontSize: '1.215rem',
+  },
+};
+
+theme.typography.h4 = {
+  fontSize: '1.44rem',
+  [theme.breakpoints.down('md')]: {
+    fontSize: '1.266rem',
+  },
+  [theme.breakpoints.down('sm')]: {
+    fontSize: '1.138rem',
+  },
+};
+
+theme.typography.h5 = {
+  fontSize: '1.2rem',
+  [theme.breakpoints.down('md')]: {
+    fontSize: '1.125rem',
+  },
+  [theme.breakpoints.down('sm')]: {
+    fontSize: '1.067rem',
+  },
+};
 
 export default theme;
