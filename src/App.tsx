66ef2b20c1ffde2cad187486a9c27ea8e39bf4a6
0qy <linqianye@tutanota.com>
2022-05-09 12:27:01 +1000

    format

--- src/App.tsx
+++ src/App.tsx
@@ -13,16 +13,6 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline>
         <NavBar />
-        <div className="App">
-          <header className="App-header">
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-              Learn React
-            </a>
-          </header>
-        </div>
         <Footer />
       </CssBaseline>
     </ThemeProvider>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -23,19 +23,17 @@ const LinkItems = styled('div')({
 
 const NavBar: React.FC = () => {
   return (
-    <>
-      <NavBarContainer>
-        <img src="/GNS.png" />
-        <LinkItems>
-          <Typography variant="h5">Hazard Curves</Typography>
-          <Typography variant="h5">Hazard Maps</Typography>
-          <Typography variant="h5">Rupture Sets</Typography>
-          <Typography variant="h5">Info &amp; Input Files</Typography>
-          <Typography variant="h5">Help</Typography>
-        </LinkItems>
-        <img src="/NSHM.png" />
-      </NavBarContainer>
-    </>
+    <NavBarContainer>
+      <img src="/GNS.png" />
+      <LinkItems>
+        <Typography variant="h5">Hazard Curves</Typography>
+        <Typography variant="h5">Hazard Maps</Typography>
+        <Typography variant="h5">Rupture Sets</Typography>
+        <Typography variant="h5">Info &amp; Input Files</Typography>
+        <Typography variant="h5">Help</Typography>
+      </LinkItems>
+      <img src="/NSHM.png" />
+    </NavBarContainer>
   );
 };
 
