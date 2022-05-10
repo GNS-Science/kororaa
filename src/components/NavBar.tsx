5d94a9bf9501fc9d48d52aad02efc54ff58c3ec7
QY <linqianye@tutanota.com>
2022-05-10 15:11:51 +1200

    responsive nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { styled } from '@mui/material/styles';
-import { Typography } from '@mui/material';
+import { AppBar, Typography, Container, Toolbar, IconButton, Box, Menu, MenuItem, Button } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
-const NavBarContainer = styled('div')(({ theme }) => ({
+const StyledAppBar = styled(AppBar)(({ theme }) => ({
   backgroundColor: theme.palette.info.main,
   height: 120,
   borderBottom: `10px solid ${theme.palette.primary.main}`,
@@ -11,28 +12,70 @@ const NavBarContainer = styled('div')(({ theme }) => ({
   justifyContent: 'space-around',
 }));
 
-const LinkItems = styled('div')({
-  height: '85%',
-  width: '60%',
-  display: 'flex',
-  flexDirection: 'row',
+const StyledToolbar = styled(Toolbar)({
   justifyContent: 'space-around',
-  alignItems: 'flex-end',
 });
 
 const NavBar: React.FC = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const pages = ['Hazard Curves', 'Hazard Maps', 'Rupture Sets', 'Info & Input Files', 'Help'];
+
   return (
-    <NavBarContainer>
-      <img src="/GNS.png" />
-      <LinkItems>
-        <Typography variant="h5">Hazard Curves</Typography>
-        <Typography variant="h5">Hazard Maps</Typography>
-        <Typography variant="h5">Rupture Sets</Typography>
-        <Typography variant="h5">Info &amp; Input Files</Typography>
-        <Typography variant="h5">Help</Typography>
-      </LinkItems>
-      <img src="/NSHM.png" />
-    </NavBarContainer>
+    <StyledAppBar position="static">
+      <Container maxWidth="xl">
+        <StyledToolbar disableGutters>
+          <img src="/GNS.png" />
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true" onClick={handleOpenNavMenu} color="inherit">
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography variant="h5" textAlign="center">
+                    {page}
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <MenuItem key={page} onClick={handleCloseNavMenu}>
+                <Typography variant="h5">{page}</Typography>
+              </MenuItem>
+            ))}
+          </Box>
+          <img src="/NSHM.png" />
+        </StyledToolbar>
+      </Container>
+    </StyledAppBar>
   );
 };
 
