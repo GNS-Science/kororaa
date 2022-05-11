1ff22b622f5acf667ca82fcb78224f4de493b808
QY <linqianye@tutanota.com>
2022-05-11 16:08:50 +1200

    disclaimer layer testing

---
+++ src/tests/components/DisclaimerLayer.test.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import DisclaimerLayer from '../../components/DisclaimerLayer';
+
+describe('For <DisclaimerLayer />: ', () => {
+  const Wrapper = () => {
+    return (
+      <DisclaimerLayer>
+        <p>hello</p>
+      </DisclaimerLayer>
+    );
+  };
+
+  const env = process.env;
+
+  beforeEach(() => {
+    jest.resetModules();
+    process.env = { ...env };
+  });
+
+  afterEach(() => {
+    process.env = env;
+  });
+
+  const user = userEvent.setup();
+
+  test('Should show disclaimer initially', () => {
+    render(<Wrapper />);
+
+    const disclaimer = screen.getByRole('presentation');
+    expect(disclaimer).toBeInTheDocument();
+    expect(screen.getByText('Disclaimer')).toBeInTheDocument();
+    expect(screen.getByRole('button')).toContainHTML('Accept');
+  });
+
+  test('Children being rendered in the background', () => {
+    render(<Wrapper />);
+
+    expect(screen.getByText('hello')).toBeInTheDocument();
+  });
+
+  test('Disclaimer should go away after clicking the Accept Button with children still rendered', async () => {
+    render(<Wrapper />);
+
+    const acceptButton = screen.getByRole('button');
+    await user.click(acceptButton);
+
+    const disclaimer = screen.queryByRole('presentation');
+    expect(disclaimer).not.toBeInTheDocument();
+    expect(screen.getByText('hello')).toBeInTheDocument();
+  });
+
+  test('LocalStorage contains correct version string after clicking Accept Button', async () => {
+    jest.spyOn(Object.getPrototypeOf(window.localStorage), 'setItem');
+    Object.setPrototypeOf(window.localStorage.setItem, jest.fn());
+    process.env.REACT_APP_DISCLAIMER_VERSION = 'testDisclaimerVersion';
+
+    render(<Wrapper />);
+
+    const acceptButton = screen.getByRole('button');
+    await user.click(acceptButton);
+
+    const disclaimerVersion = localStorage.getItem('disclaimer-version');
+    expect(disclaimerVersion).toMatch('testDisclaimerVersion');
+    expect(window.localStorage.setItem).toHaveBeenCalledWith('disclaimer-version', 'testDisclaimerVersion');
+  });
+});
