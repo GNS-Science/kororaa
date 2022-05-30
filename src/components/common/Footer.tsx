9fcace203207f9967a28953b009b218a6b1ec61b
QY <linqianye@tutanota.com>
2022-05-31 10:05:12 +1200

    sticky footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -3,11 +3,10 @@ import { styled } from '@mui/material/styles';
 import { Typography } from '@mui/material';
 import PartnersLogos from './PartnersLogos';
 
-const FooterContainer = styled('div')(({ theme }) => ({
+const FooterContainer = styled('footer')(({ theme }) => ({
+  flexShrink: 0,
   padding: 40,
   height: 250,
-  position: 'relative',
-  bottom: 0,
   width: '100%',
   display: 'flex',
   flexDirect: 'row',
