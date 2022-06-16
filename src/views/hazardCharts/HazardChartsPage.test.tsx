245265bdf907f4fab67986a35536c42390faa089
0qy <linqianye@tutanota.com>
2022-06-16 15:11:00 +1200

    add testing comment

--- src/views/hazardCharts/HazardChartsPage.test.tsx
+++ src/views/hazardCharts/HazardChartsPage.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { RelayEnvironmentProvider } from 'react-relay';
-import userEvent from '@testing-library/user-event';
 
 import HazardChartsPage from './HazardChartsPage';
 import RelayEnvironment from '../../RelayEnvironment';
@@ -18,6 +17,10 @@ describe('For HazardChartsPage component', () => {
   it('User can see heading and a list of controls, a query is automatically loaded, with hazard curve showing, and spectra curve not showing', async () => {
     render(<TestRender />);
 
+    //Responsive charts from toshi-nest has not been able to render in jsdom testing environments
+    //This is to do with getBoundingClientRect and ResizeObserver not being available in this environment
+    //This gap is filled by cypress testing, becauset here is a browser window the responsive charts renders as expected
+    //So for now this test will test for the correct legen label only.
     const heading = screen.getByRole('heading');
     const buttons = screen.getAllByRole('button');
     const charts = await screen.findByRole('plotsView');
