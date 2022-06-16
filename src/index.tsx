0d2dab1f152297f78c0a77a3b701983452b36054
0qy <linqianye@tutanota.com>
2022-06-16 15:30:21 +1200

    add msw comment to index

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { worker } from './mocks/browser';
+//uncommnet lines below to start msw for browser
+// import { worker } from './mocks/browser';
 
-if (process.env.NODE_ENV === 'development') {
-  worker.start();
-}
+// if (process.env.NODE_ENV === 'development') {
+//   worker.start();
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
