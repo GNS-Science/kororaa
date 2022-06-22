7729c2116ca4121abcd934fe049caae0ecc833a4
benjamineac <ben.albrecht.chamberlain@gmail.com>
2022-06-22 16:22:49 +1200

    update to nest 1.4.2;

--- src/views/ruptureSets/RuptureSetsPage.tsx
+++ src/views/ruptureSets/RuptureSetsPage.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { geojsonMockData } from './geojsonMockData';
-import { LeafletMap } from '@gns-science/toshi-nest';
+import { LeafletMap, LeafletDrawer } from '@gns-science/toshi-nest';
 
 const RuptureSetsPage: React.FC = () => {
+  const [fullscreen, setFullscreen] = React.useState<boolean>(false);
   const rupturesData = geojsonMockData.ruptures;
   const locationsData = geojsonMockData.locations;
-  const providerUrl = 'https://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}';
   const showLocation = true;
   const zoom = 5;
   const nzCentre = [-40.946, 174.167];
 
-  return <LeafletMap zoom={zoom} nzCentre={nzCentre} providerUrl={providerUrl} rupturesData={rupturesData} locationsData={locationsData} showLocation={showLocation} />;
+  return (
+    <>
+      <LeafletMap zoom={zoom} nzCentre={nzCentre} rupturesData={rupturesData} locationsData={locationsData} showLocation={showLocation} height={'700px'} width={'100%'} setFullscreen={setFullscreen} />
+      <LeafletDrawer drawerHeight={'700px'} headerHeight={'120px'} width={'400px'} fullscreen={fullscreen}></LeafletDrawer>
+    </>
+  );
 };
 
 export default RuptureSetsPage;
