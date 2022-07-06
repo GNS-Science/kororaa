7582e5bf3559d4a3c99d6cfa308b42d0aba4a0fe
benjamineac <ben.albrecht.chamberlain@gmail.com>
2022-07-06 14:47:25 +1200

    extends hazard curve test coverage;

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,8 +1,22 @@
 import { graphql } from 'msw';
-import { hazardChartsPlotsViewMockData } from './mockData/HazardChartsPlotsViewMockData';
+import { hazardChartsPlotsViewMockDataWLG } from './mockData/HazardChartsPlotsViewMockDataWLG';
+import { hazardChartsPlotsViewMockDataWLGCHC } from './mockData/HazardChartsPlotsViewMockDataWLGCHC';
+import { hazardChartsPlotsViewMockDataWLGCHCVS30 } from './mockData/HazardChartsPlotsViewMockDataWLGCHCVS30';
 
 export const handlers = [
   graphql.query('HazardChartsPlotsViewQuery', (req, res, ctx) => {
-    return res(ctx.data(hazardChartsPlotsViewMockData));
+    const locations = req.variables.locs;
+    const vs30 = req.variables.vs30s;
+    if (locations.includes('WLG') && locations.includes('CHC')) {
+      if (vs30.includes(250) && vs30.includes(350)) {
+        return res(ctx.data(hazardChartsPlotsViewMockDataWLGCHCVS30));
+      }
+      if (vs30.includes(250)) {
+        return res(ctx.data(hazardChartsPlotsViewMockDataWLGCHC));
+      }
+    }
+    if (locations.includes('WLG')) {
+      return res(ctx.data(hazardChartsPlotsViewMockDataWLG));
+    }
   }),
 ];
