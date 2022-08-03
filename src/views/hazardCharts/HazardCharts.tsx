8ea82dd616933c8adba6a7d4c3fdd5763dde367b
0qy <linqianye@tutanota.com>
2022-08-04 11:22:29 +1200

    update toshi nest and use GroupCurveChartResponsive for spectra

--- src/views/hazardCharts/HazardCharts.tsx
+++ src/views/hazardCharts/HazardCharts.tsx
@@ -1,11 +1,12 @@
 import React, { useMemo } from 'react';
 import { Box } from '@mui/material';
 import { styled } from '@mui/material/styles';
-import { SpectralAccelChartResponsive, HazardUncertaintyChartResponsive } from '@gns-science/toshi-nest';
+import { GroupCurveChartResponsive } from '@gns-science/toshi-nest';
 
 import { HazardChartsPlotsViewQuery$data } from './__generated__/HazardChartsPlotsViewQuery.graphql';
-import { getAllCurveGroups, addCurveGroupColors, getSAColors, getFilteredCurveGroups, getSpectralAccelerationCurvesUncertainty } from './hazardPage.service';
+import { getAllCurveGroups, addCurveGroupColors, getFilteredCurveGroups } from './hazardPage.service';
 import { HazardPageState } from './hazardPageReducer';
+import { addColorsToCurves, getSpectralAccelUncertaintyCurves } from '../../services/spectralAccel/spectralAccel.service';
 
 interface HazardChartsProps {
   data: HazardChartsPlotsViewQuery$data;
@@ -16,8 +17,11 @@ const HazardCharts: React.FC<HazardChartsProps> = ({ data, state }: HazardCharts
   const allCurveGroups = useMemo(() => getAllCurveGroups(data), [data]);
   const filteredCurveGroups = useMemo(() => getFilteredCurveGroups(allCurveGroups, state.imts), [allCurveGroups, state.imts]);
   const curveGroupWithColors = useMemo(() => addCurveGroupColors(filteredCurveGroups), [filteredCurveGroups]);
-  const SAcurvesUncertainty = useMemo(() => getSpectralAccelerationCurvesUncertainty(allCurveGroups, state.vs30s, state.locs, state.poe), [allCurveGroups, state]);
-  const SAcurvesColors = useMemo(() => getSAColors(SAcurvesUncertainty, filteredCurveGroups), [SAcurvesUncertainty, filteredCurveGroups]);
+  // const SAcurvesUncertainty = useMemo(() => getSpectralAccelerationCurvesUncertainty(allCurveGroups, state.vs30s, state.locs, state.poe), [allCurveGroups, state]);
+  // const SAcurvesColors = useMemo(() => getSAColors(SAcurvesUncertainty, filteredCurveGroups), [SAcurvesUncertainty, filteredCurveGroups]);
+  const saCurvesUncertainty = useMemo(() => getSpectralAccelUncertaintyCurves(state.vs30s, state.locs, data, state.poe), [state, data]);
+  const saCurvesWithColors = useMemo(() => addColorsToCurves(saCurvesUncertainty), [saCurvesUncertainty]);
+
   const HazardChartsContainer = styled(Box)(({ theme }) => ({
     display: 'flex',
     padding: 20,
@@ -39,7 +43,7 @@ const HazardCharts: React.FC<HazardChartsProps> = ({ data, state }: HazardCharts
     <>
       <HazardChartsContainer data-testid="hazardChartsContainer">
         <ChartContainer>
-          <HazardUncertaintyChartResponsive
+          <GroupCurveChartResponsive
             scaleType="log"
             xLimits={[0.01, 10]}
             yLimits={[0.000001, 1]}
@@ -49,11 +53,26 @@ const HazardCharts: React.FC<HazardChartsProps> = ({ data, state }: HazardCharts
             subHeading={`${state.imts[0]}`}
             curves={curveGroupWithColors}
             poe={state.poe}
+            uncertainty="true"
           />
         </ChartContainer>
         {state.poe && (
           <ChartContainer>
-            <SpectralAccelChartResponsive testId="sa-chart" data={SAcurvesUncertainty} colors={SAcurvesColors} heading={'Spectral Acceleration Chart'} subHeading={`${state.poe * 100}% in 50 years`} />
+            {/* <SpectralAccelChartResponsive testId="sa-chart" data={SAcurvesUncertainty} colors={SAcurvesColors} heading={'Spectral Acceleration Chart'} subHeading={`${state.poe * 100}% in 50 years`} /> */}
+            <GroupCurveChartResponsive
+              testId="sa-chart"
+              scaleType="linear"
+              yScaleType="linear"
+              xLimits={[-1, 6]}
+              yLimits={[0, 4]}
+              tooltip={true}
+              crosshair={true}
+              heading="Spectral Acceleration Chart"
+              subHeading={`${state.poe * 100}% in 50 years`}
+              curves={saCurvesWithColors}
+              poe={state.poe}
+              uncertainty="true"
+            />
           </ChartContainer>
         )}
       </HazardChartsContainer>
