b0eafc7e957fbc300fb29ae64df857559ecb1738
QY <linqianye@tutanota.com>
2022-08-11 10:10:23 +1200

    remove obsolete service functions

--- src/views/hazardCharts/hazardPage.service.ts
+++ src/views/hazardCharts/hazardPage.service.ts
@@ -1,7 +1,5 @@
-import * as mathjs from 'mathjs';
 import { colorSet } from './constants/hazardCharts';
-import { hazardPageLocations, hazardPageOptions } from './constants/hazardPageOptions';
-
+import { hazardPageLocations } from './constants/hazardPageOptions';
 import { HazardChartsPlotsViewQuery$data } from './__generated__/HazardChartsPlotsViewQuery.graphql';
 
 export interface XY {
@@ -98,155 +96,37 @@ const getAggValue = (agg: string): string => {
   }
 };
 
-const getImtValue = (imt: string): string => {
-  switch (imt) {
-    case 'PGA':
-      return '0.01';
-    case 'SA(0.1)':
-      return '0.1';
-    case 'SA(0.2)':
-      return '0.2';
-    case 'SA(0.3)':
-      return '0.3';
-    case 'SA(0.4)':
-      return '0.4';
-    case 'SA(0.5)':
-      return '0.5';
-    case 'SA(0.7)':
-      return '0.7';
-    case 'SA(1.0)':
-      return '1.0';
-    case 'SA(1.5)':
-      return '1.5';
-    case 'SA(2.0)':
-      return '2.0';
-    case 'SA(3.0)':
-      return '3.0';
-    case 'SA(4.0)':
-      return '4.0';
-    case 'SA(5.0)':
-      return '5.0';
-    case 'SA(1)':
-      return '1';
-    default:
-      return '0.01';
-  }
-};
+//TODO: CSV data feature will be reindated in a future feature
+// export const getCSVdata = (PGAoptions: string[], data: HazardChartsPlotsViewQuery$data): string[][] => {
+//   const allCurves = getAllCurves(data);
+//   const CSVdata: string[][] = [];
+//   CSVdata[0] = getCSVheadings(PGAoptions);
 
-export const getSpectralAccelerationCurvesUncertainty = (curveGroups: HazardUncertaintyChartData, vs30s: number[], locations: string[], poe: number | undefined): Record<string, XY[]> => {
-  const meanCurves: Record<string, XY[]> = {};
+//   allCurves.PGA.map((xy) => {
+//     const row: string[] = [];
+//     row.push(xy.x.toExponential());
 
-  Object.keys(curveGroups).forEach((key) => {
-    const curve: XY[] = [];
-    curveGroups[key]['mean'].data.forEach((point) => {
-      curve.push({ x: point[0], y: point[1] });
-    });
-    meanCurves[key] = curve;
-  });
+//     for (const key in allCurves) {
+//       const position = CSVdata[0].findIndex((acceleration) => acceleration.includes(key));
+//       const currentPoint = allCurves[key].find((curve) => curve.x === xy.x);
+//       row.splice(position, 0, (currentPoint?.y as number).toExponential());
+//     }
 
-  const saCurves = getSpectralAccelerationCurves(meanCurves, vs30s, locations, poe);
+//     CSVdata.push(row);
+//   });
 
-  return saCurves;
-};
+//   return CSVdata;
+// };
 
-export const getSpectralAccelerationCurves = (allCurves: Record<string, XY[]>, vs30s: number[], locations: string[], poe: number | undefined): Record<string, XY[]> => {
-  const curves: Record<string, XY[]> = {};
+// const getCSVheadings = (PGAoptions: string[]): string[] => {
+//   const headings: string[] = [];
+//   headings[0] = 'acceleration';
+//   PGAoptions.map((item) => {
+//     headings.push(`PoE(${item})`);
+//   });
 
-  poe &&
-    vs30s.forEach((vs30) => {
-      locations.forEach((location) => {
-        const curvesArray: Record<string, XY[]> = {};
-
-        for (const key in allCurves) {
-          if (key.includes(vs30.toString()) && key.includes(location)) {
-            curvesArray[key] = allCurves[key];
-          }
-        }
-
-        const curve = getSpectralAccelerationData(hazardPageOptions.imts, poe, curvesArray);
-        curves[`${vs30}m/s ${location}`] = curve;
-      });
-    });
-
-  return curves;
-};
-
-export const getSAColors = (curves: Record<string, XY[]>, curveGroups: HazardUncertaintyChartData): Record<string, string> => {
-  const colors: Record<string, string> = {};
-  Object.keys(curves).forEach((key: string) => {
-    const curveGroup = Object.keys(curveGroups).find((curveGroupsKey) => curveGroupsKey.includes(key));
-
-    if (curveGroup) {
-      const strokeColor = curveGroups[curveGroup]['mean'].strokeColor ?? '#000000';
-      colors[key] = strokeColor;
-    }
-  });
-
-  return colors;
-};
-
-export const getSpectralAccelerationData = (imtValues: string[], poe: number | undefined, filteredCurves: Record<string, XY[]>): XY[] => {
-  const dataSet: XY[] = [];
-  if (poe) {
-    const xValue: number = -Math.log(1 - poe) / 50;
-
-    for (const key in filteredCurves) {
-      const imt = imtValues.find((imt) => key.includes(imt));
-
-      try {
-        let p1: number[] = [];
-        let p2: number[] = [];
-        const p3 = [Math.log(2e-3), Math.log(xValue)];
-        const p4 = [Math.log(10), Math.log(xValue)];
-
-        filteredCurves[key].find((xy, i) => {
-          if (xy.y <= xValue) {
-            p1 = [Math.log(xy.x), Math.log(xy.y)];
-            p2 = [Math.log(filteredCurves[key][i - 1].x), Math.log(filteredCurves[key][i - 1].y)];
-            return true;
-          }
-        });
-        const point = mathjs.intersect(p1, p2, p3, p4);
-        const result = [Math.exp(point[0] as number), mathjs.exp(mathjs.exp(point[1] as number))];
-        dataSet.push({ x: imt === 'PGA' ? 0.01 : parseFloat(getImtValue(imt as string)), y: result[0] });
-      } catch {
-        dataSet.push({ x: imt === 'PGA' ? 0.01 : parseFloat(getImtValue(imt as string)), y: 0 });
-      }
-    }
-  }
-  return dataSet;
-};
-
-export const getCSVdata = (PGAoptions: string[], data: HazardChartsPlotsViewQuery$data): string[][] => {
-  const allCurves = getAllCurves(data);
-  const CSVdata: string[][] = [];
-  CSVdata[0] = getCSVheadings(PGAoptions);
-
-  allCurves.PGA.map((xy) => {
-    const row: string[] = [];
-    row.push(xy.x.toExponential());
-
-    for (const key in allCurves) {
-      const position = CSVdata[0].findIndex((acceleration) => acceleration.includes(key));
-      const currentPoint = allCurves[key].find((curve) => curve.x === xy.x);
-      row.splice(position, 0, (currentPoint?.y as number).toExponential());
-    }
-
-    CSVdata.push(row);
-  });
-
-  return CSVdata;
-};
-
-const getCSVheadings = (PGAoptions: string[]): string[] => {
-  const headings: string[] = [];
-  headings[0] = 'acceleration';
-  PGAoptions.map((item) => {
-    headings.push(`PoE(${item})`);
-  });
-
-  return headings;
-};
+//   return headings;
+// };
 
 export const convertLocationsToIDs = (locations: string[]): string[] => {
   const locationIDs: string[] = [];
@@ -294,51 +174,4 @@ export const numbersToStrings = (values: number[]) => {
 
 export const stringsToNumbers = (values: string[]) => {
   return values.map((value) => Number(value));
-};
-
-// For Hazard Charts without uncertainty
-export const getAllCurves = (data: HazardChartsPlotsViewQuery$data): Record<string, XY[]> => {
-  const curves: Record<string, XY[]> = {};
-
-  data.hazard_curves?.curves?.forEach((currentCurve) => {
-    const imt = currentCurve?.imt;
-    const levels = currentCurve?.curve?.levels;
-    const values = currentCurve?.curve?.values;
-
-    if (imt && levels && values) {
-      const curveName = `${currentCurve.vs30}m/s ${currentCurve.loc} ${currentCurve.imt} ${currentCurve.agg}`;
-      const curve: XY[] = [];
-
-      levels.forEach((level, index) => {
-        curve.push({ x: level as number, y: values[index] as number });
-      });
-
-      curves[curveName] = curve;
-    }
-  });
-
-  return curves;
-};
-
-export const getFilteredCurves = (curves: Record<string, XY[]>, imts: string[]): Record<string, XY[]> => {
-  const newCurves: Record<string, XY[]> = {};
-
-  imts.forEach((imt) => {
-    for (const key in curves) {
-      if (key.includes(imt)) {
-        newCurves[key] = curves[key];
-      }
-    }
-  });
-
-  return newCurves;
-};
-
-export const getColors = (curve: Record<string, XY[]>): Record<string, string> => {
-  const colors: Record<string, string> = {};
-  for (const key in curve) {
-    colors[key] = '#000000';
-  }
-
-  return colors;
 };
--- src/views/hazardCharts/tests/hazardPage.service.test.ts
+++ src/views/hazardCharts/tests/hazardPage.service.test.ts
@@ -1,21 +1,6 @@
 import * as hazardPageService from '../hazardPage.service';
 import { HazardChartsPlotsViewQuery$data } from '../__generated__/HazardChartsPlotsViewQuery.graphql';
 
-describe('For the getAllCurves function', () => {
-  it('should return an empty object if the data is empty', () => {
-    const data: HazardChartsPlotsViewQuery$data = { hazard_curves: null };
-    const curves = hazardPageService.getAllCurves(data);
-
-    expect(curves).toEqual({});
-  });
-
-  it('should return an empty object if curves is empty', () => {
-    const data: HazardChartsPlotsViewQuery$data = { hazard_curves: { ok: true, curves: null } };
-    const curves = hazardPageService.getAllCurves(data);
-
-    expect(curves).toEqual({});
-  });
-});
 //   it('should return the curve in xy format if imt, levels, and values are all present', () => {
 //     const data: HazardChartsPlotsViewQuery$data = {
 //       hazard_curves: {
@@ -75,34 +60,6 @@ describe('For the getAllCurves function', () => {
 //     });
 //   });
 // });
-
-describe('For the getCurve function', () => {
-  it('should return an empty object if the data is empty', () => {
-    const data: Record<string, hazardPageService.XY[]> = {};
-    const curve = hazardPageService.getFilteredCurves(data, ['1']);
-
-    expect(curve).toEqual({});
-  });
-
-  it('should return the curve where imt prop matches the key', () => {
-    const curves = {
-      '1': [{ x: 1, y: 1 }],
-      '2': [{ x: 2, y: 2 }],
-      '3': [{ x: 3, y: 3 }],
-    };
-    const curve = hazardPageService.getFilteredCurves(curves, ['3']);
-
-    expect(curve).toEqual({ '3': [{ x: 3, y: 3 }] });
-  });
-});
-
-describe('For the getColor function', () => {
-  it('should return the color for the given imt', () => {
-    const color = hazardPageService.getColors({ '1': [{ x: 1, y: 1 }] });
-
-    expect(color).toEqual({ '1': '#000000' });
-  });
-});
 
 test.todo('For the getSpectralAccelerationData function');
 test.todo('For the getImtValue function');
