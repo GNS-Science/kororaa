cb67b6d754e7ab0ac183550b928a4334e5041b97
QY <linqianye@tutanota.com>
2022-08-11 10:06:39 +1200

    remove unused service function

--- src/services/latLon/latLon.service.test.ts
+++ src/services/latLon/latLon.service.test.ts
@@ -1,22 +1,5 @@
 import * as latLonService from './latLon.service';
 
-describe('FormatLatLon function formats latLon correctly', () => {
-  it('coordinates formats properly', () => {
-    const result = latLonService.formatLatLon('-45.212,170.516457780');
-    expect(result).toBe('-45.212~170.516457780');
-  });
-
-  it(' 1 , 2 becomes 1~2, spaces are handled correctly', () => {
-    const result = latLonService.formatLatLon('1 , 2');
-    expect(result).toBe('1~2');
-  });
-
-  it(' 1 ,2 becomes 1~2', () => {
-    const result = latLonService.formatLatLon('1 ,2');
-    expect(result).toBe('1~2');
-  });
-});
-
 describe('getlatlonObject function', () => {
   it('123~123', () => {
     const result = latLonService.getLatlonObject('111.000~222.000');
