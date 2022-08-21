316b9e5acf3fc8d74437c68fd4b383704917ae49
QY <linqianye@tutanota.com>
2022-08-22 10:15:28 +1200

    single select & styleUtils

--- src/views/hazardMaps/__generated__/HazardMapsControls.tsx
+++ src/views/hazardMaps/__generated__/HazardMapsControls.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { Box, Button } from '@mui/material';
+import { SelectControl } from '@gns-science/toshi-nest';
 
-import SelectControlMultiple from '../../../components/common/SelectControlMultiple';
 import { hazardPageOptions } from '../../hazardCharts/constants/hazardPageOptions';
 import { flexParentCenter } from '../../../utils/styleUtils';
-import { numbersToStrings, stringsToNumbers } from '../../hazardCharts/hazardPage.service';
+import { numbersToStrings } from '../../hazardCharts/hazardPage.service';
 import { HazardMapsState } from '../hazardMapReducer';
 
 interface HazardMapsControlsProps {
@@ -13,26 +13,21 @@ interface HazardMapsControlsProps {
 }
 
 const HazardMapsControls: React.FC<HazardMapsControlsProps> = ({ state, dispatch }: HazardMapsControlsProps) => {
-  const [imts, setImts] = useState<string[]>(state.imts);
-  const [aggs, setAggs] = useState<string[]>(state.aggs);
-  const [vs30s, setVs30s] = useState<number[]>(state.vs30s);
-  const [poes, setPoes] = useState<number[]>(state.poes);
+  const [imts, setImts] = useState<string>(state.imts[0]);
+  const [aggs, setAggs] = useState<string>(state.aggs[0]);
+  const [vs30s, setVs30s] = useState<number>(state.vs30s[0]);
+  const [poes, setPoes] = useState<number>(state.poes[0]);
 
   const handleSubmit = () => {
-    dispatch({ imts, aggs, vs30s, poes });
+    dispatch({ imts: [imts], aggs: [aggs], vs30s: [vs30s], poes: [poes] });
   };
 
   return (
     <Box sx={{ width: '100%', ...flexParentCenter, flexDirection: 'column' }}>
-      <SelectControlMultiple name="Imts" options={['PGA', 'SA(0.1)', 'SA(0.5)', 'SA(1.0)']} selection={imts} setSelection={setImts} />
-      <SelectControlMultiple name="Aggs" options={['mean']} selection={aggs} setSelection={setAggs} />
-      <SelectControlMultiple
-        name="Vs30s"
-        options={numbersToStrings(hazardPageOptions.vs30s)}
-        selection={numbersToStrings(vs30s)}
-        setSelection={(newValue: string[]) => setVs30s(stringsToNumbers(newValue))}
-      />
-      <SelectControlMultiple name="poes" options={['0.1', '0.02']} selection={numbersToStrings(poes)} setSelection={(newValue: string[]) => setPoes(stringsToNumbers(newValue))} />
+      <SelectControl name="Imts" options={['PGA', 'SA(0.1)', 'SA(0.5)', 'SA(1.0)']} selection={imts} setSelection={setImts} />
+      <SelectControl name="Aggs" options={['mean']} selection={aggs} setSelection={setAggs} />
+      <SelectControl name="Vs30s" options={numbersToStrings(hazardPageOptions.vs30s)} selection={vs30s.toString()} setSelection={(newValue: string[]) => setVs30s(Number(newValue))} />
+      <SelectControl name="poes" options={['0.1', '0.02']} selection={poes.toString()} setSelection={(newValue: string[]) => setPoes(Number(newValue))} />
       <Button variant="contained" type="submit" onClick={handleSubmit}>
         Submit
       </Button>
