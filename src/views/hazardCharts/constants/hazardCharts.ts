c46e3b9777812501916ee4d2b61145df1ff61f49
QY <linqianye@tutanota.com>
2022-08-31 10:46:08 +1200

    env update

--- src/views/hazardCharts/constants/hazardCharts.ts
+++ src/views/hazardCharts/constants/hazardCharts.ts
@@ -1,5 +1,5 @@
-import { COLOR_LIMIT } from '../../../utils/environmentVariables';
+import { HAZARD_COLOR_LIMIT } from '../../../utils/environmentVariables';
 
 export const poeInputLabel = 'Probability of Exeedance (50 Yrs)';
 
-export const tooManyCurves = `Maximum of ${COLOR_LIMIT} curves can be shown together. Please select fewer options.`;
+export const tooManyCurves = `Maximum of ${HAZARD_COLOR_LIMIT} curves can be shown together. Please select fewer options.`;
