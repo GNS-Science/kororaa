a892b7a24ebc1c27b697d0eefd567179982ad62d
QY <linqianye@tutanota.com>
2022-09-01 11:52:31 +1200

    code cleanup

--- src/views/hazardMaps/HazardMaps.tsx
+++ src/views/hazardMaps/HazardMaps.tsx
@@ -1,23 +1,13 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { LeafletMap, ColorBar } from '@gns-science/toshi-nest';
 import { Box } from '@mui/material';
-import { graphql } from 'babel-plugin-relay/macro';
 
-import { HazardMapsState } from './hazardMapReducer';
-import { useLazyLoadQuery } from 'react-relay';
-// import { HazardMapsQuery } from './__generated__/HazardMapsQuery.graphql';
-import { getTickValues, readablePoe } from './hazardMaps.service';
-import { GRID_ID, HAZARD_MODEL } from '../../utils/environmentVariables';
+import { ColorScale } from './hazardMaps.service';
 
 interface HazardMapsProps {
   geoJson: string[];
   colorScale: ColorScale | undefined;
   setFullscreen: (value: boolean) => void;
-}
-
-export interface ColorScale {
-  levels: number[];
-  hexrgbs: (string | undefined)[];
 }
 
 const HazardMaps: React.FC<HazardMapsProps> = ({ geoJson, setFullscreen, colorScale }: HazardMapsProps) => {
@@ -35,36 +25,3 @@ const HazardMaps: React.FC<HazardMapsProps> = ({ geoJson, setFullscreen, colorSc
 };
 
 export default HazardMaps;
-
-// export const hazardMapsQuery = graphql`
-//   query HazardMapsQuery(
-//     $grid_id: RegionGrid
-//     $hazard_model_ids: [String]
-//     $imts: [String]
-//     $aggs: [String]
-//     $vs30s: [Float]
-//     $poes: [Float]
-//     $color_scale: String
-//     $color_scale_vmax: Float
-//     $fill_opacity: Float
-//     $stroke_width: Float
-//     $stroke_opacity: Float
-//   ) {
-//     gridded_hazard(grid_id: $grid_id, hazard_model_ids: $hazard_model_ids, imts: $imts, aggs: $aggs, vs30s: $vs30s, poes: $poes) {
-//       ok
-//       gridded_hazard {
-//         grid_id
-//         hazard_model
-//         imt
-//         agg
-//         hazard_map(color_scale: $color_scale, color_scale_vmax: $color_scale_vmax, fill_opacity: $fill_opacity, stroke_width: $stroke_width, stroke_opacity: $stroke_opacity) {
-//           geojson
-//           colour_scale {
-//             levels
-//             hexrgbs
-//           }
-//         }
-//       }
-//     }
-//   }
-// `;
