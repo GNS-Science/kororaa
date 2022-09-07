5650b37f20f8d6a5887e9fcd4dea5f33f1f81670
Chris B Chamberlain <chrisbc@artisan.co.nz>
2022-09-07 18:11:14 +1200

    Feature/104 navbar improvements part1 (#126)
    * refactor navbar menus into hamburger and main;
    * almost working main menu....
    * navbar sub menus work;
    
    Co-authored-by: ben-chamberlain <ben.albrecht.chamberlain@gmail.com>

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -16,23 +16,143 @@ const StyledToolbar = styled(Toolbar)({
   justifyContent: 'space-around',
 });
 
-const NavBar: React.FC = () => {
+interface MenuPageItem {
+  name: string;
+  path?: string;
+  submenu?: MenuPageItem[];
+}
+
+interface MenuProps {
+  pages: MenuPageItem[];
+}
+
+interface SubMenuProps {
+  pages: MenuPageItem[];
+  anchorElNav: HTMLElement | null;
+  setAnchorElNav: React.Dispatch<React.SetStateAction<HTMLElement | null>>;
+  open: boolean;
+}
+
+interface FluidMenuProps {
+  page: MenuPageItem;
+}
+
+const SubMenu: React.FC<SubMenuProps> = ({ pages, anchorElNav, setAnchorElNav, open }: SubMenuProps) => {
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <Menu
+      id="menu-appbar"
+      anchorEl={anchorElNav}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'left',
+      }}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'left',
+      }}
+      open={open}
+      onClose={handleCloseNavMenu}
+    >
+      {pages.map((page) => (
+        <MenuItem key={page.name} onClick={handleCloseNavMenu} component={Link} href={page.path}>
+          <Typography variant="h5" textAlign="center">
+            {page.name}
+          </Typography>
+        </MenuItem>
+      ))}
+    </Menu>
+  );
+};
+
+const HamburgerMenu: React.FC<MenuProps> = ({ pages }: MenuProps) => {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
   };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+  return (
+    <>
+      <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true" onClick={handleOpenNavMenu} color="inherit">
+        <MenuIcon />
+      </IconButton>
+      <SubMenu open={Boolean(anchorElNav)} pages={pages} anchorElNav={anchorElNav} setAnchorElNav={setAnchorElNav} />
+    </>
+  );
+};
 
+const FluidMenuItem: React.FC<FluidMenuProps> = ({ page }: FluidMenuProps) => {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  if (page.path) {
+    return (
+      <MenuItem key={page.name} component={Link} href={page.path}>
+        <Typography variant="h5" textAlign="center">
+          {page.name}
+        </Typography>
+      </MenuItem>
+    );
+  }
+  return (
+    <>
+      <MenuItem onClick={handleOpenNavMenu} key={page.name} component={Link} href={page.path}>
+        <Typography variant="h5" textAlign="center">
+          {page.name}
+        </Typography>
+      </MenuItem>
+      {page.submenu && <SubMenu open={Boolean(anchorElNav)} pages={page.submenu} anchorElNav={anchorElNav} setAnchorElNav={setAnchorElNav} />}
+    </>
+  );
+};
+
+const MainMenu: React.FC<MenuProps> = ({ pages }: MenuProps) => {
+  return (
+    <>
+      {pages.map((page) => (
+        <FluidMenuItem key={page.name} page={page} />
+      ))}
+    </>
+  );
+};
+
+const NavBar: React.FC = () => {
   const pages = [
+    {
+      name: 'Site Hazard',
+      submenu: [
+        { name: 'Hazard Curves', path: '/HazardCurves' },
+        { name: 'Disaggregations', path: '/Disaggs' },
+      ],
+    },
+    { name: 'Hazard Maps', path: '/HazardMaps' },
+    { name: 'Coming Features', path: '/Previews' },
+    { name: 'Resources', path: '/Resources' },
+    {
+      name: 'Help',
+      submenu: [
+        { name: 'Help', path: '/Help' },
+        { name: 'About', path: '/About' },
+        { name: 'Contacts', path: '/Contacts' },
+      ],
+    },
+  ];
+
+  const hamburgerPages = [
     { name: 'Hazard Curves', path: '/HazardCurves' },
+    { name: 'Disaggregations', path: '/Disaggs' },
     { name: 'Hazard Maps', path: '/HazardMaps' },
     { name: 'Coming Features', path: '/Previews' },
     { name: 'Resources', path: '/Resources' },
     { name: 'Help', path: '/Help' },
+    { name: 'About', path: '/About' },
+    { name: 'Contacts', path: '/Contacts' },
   ];
 
   return (
@@ -45,44 +165,10 @@ const NavBar: React.FC = () => {
               {/*<img src="/images/NSHM_logo_black.png" height="70" alt="NSHM logo" />*/}
             </Link>
             <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-              <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true" onClick={handleOpenNavMenu} color="inherit">
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'left',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'left',
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: 'block', md: 'none' },
-                }}
-              >
-                {pages.map((page) => (
-                  <MenuItem key={page.name} onClick={handleCloseNavMenu} component={Link} href={page.path}>
-                    <Typography variant="h5" textAlign="center">
-                      {page.name}
-                    </Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
+              <HamburgerMenu pages={hamburgerPages} />
             </Box>
             <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-              {pages.map((page) => (
-                <MenuItem key={page.name} onClick={handleCloseNavMenu} component={Link} href={page.path}>
-                  <Typography variant="h5" textAlign="center">
-                    {page.name}
-                  </Typography>
-                </MenuItem>
-              ))}
+              <MainMenu pages={pages} />
             </Box>
             {/*<CardMedia component="img" height="100" image="/images/2GNS_logo_HORZ.png" alt="GNS logo" />*/}
             <img src="/images/2GNS_logo_HORZ.png" height="80" alt="GNS logo" />
