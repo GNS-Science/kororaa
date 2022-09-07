6a03b5cc57f8570bd04656b44c140c738a7d0203
QY <linqianye@tutanota.com>
2022-09-08 10:37:50 +1200

    updated colobar with heading and background

--- src/views/hazardMaps/HazardMaps.tsx
+++ src/views/hazardMaps/HazardMaps.tsx
@@ -3,14 +3,16 @@ import { LeafletMap, ColorBar } from '@gns-science/toshi-nest';
 import { Box } from '@mui/material';
 
 import { ColorScale } from './hazardMaps.service';
+import { HazardMapsState } from './hazardMapReducer';
 
 interface HazardMapsProps {
+  state: HazardMapsState;
   geoJson: string[];
   colorScale: ColorScale | undefined;
   setFullscreen: (value: boolean) => void;
 }
 
-const HazardMaps: React.FC<HazardMapsProps> = ({ geoJson, setFullscreen, colorScale }: HazardMapsProps) => {
+const HazardMaps: React.FC<HazardMapsProps> = ({ state, geoJson, setFullscreen, colorScale }: HazardMapsProps) => {
   const zoom = 5;
   const nzCentre = [-40.946, 174.167];
 
@@ -18,7 +20,14 @@ const HazardMaps: React.FC<HazardMapsProps> = ({ geoJson, setFullscreen, colorSc
     <Box id="map" sx={{ width: '100%', height: '700px' }}>
       <LeafletMap geoJsonData={geoJson} zoom={zoom} nzCentre={nzCentre} height={'700px'} width={'100%'} setFullscreen={setFullscreen} />
       {colorScale && (
-        <ColorBar width={300} height={35} colors={colorScale?.hexrgbs} tickValues={colorScale?.levels} style={{ position: 'relative', zIndex: 10000000, top: '-125px', left: 'calc(100% - 365px)' }} />
+        <ColorBar
+          heading={`Vs30=${state.vs30[0]}m/s, ${state.spectralPeriod[0]} ${state.poe[0] * 100}% in 50 years`}
+          width={276}
+          height={35}
+          colors={colorScale?.hexrgbs}
+          tickValues={colorScale?.levels}
+          style={{ position: 'relative', zIndex: 1197, top: '-115px', left: 'calc(100% - 319px)' }}
+        />
       )}
     </Box>
   );
