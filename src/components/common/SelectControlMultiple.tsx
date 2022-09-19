e81ab30c19035ad6d5edb4f7c5edae2c2eb09ecb
ben-chamberlain <ben.albrecht.chamberlain@gmail.com>
2022-09-19 17:26:05 +1200

    hazard curve page has control tooltips;

--- src/components/common/SelectControlMultiple.tsx
+++ src/components/common/SelectControlMultiple.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Checkbox, FormControl, Input, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
+import { Checkbox, FormControl, Input, InputLabel, MenuItem, Select, SelectChangeEvent, Tooltip } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 const MultiSelectContainer = styled('div')({
@@ -13,40 +13,72 @@ export interface SelectControlMultiple {
   selection: string[];
   setSelection: (selections: string[]) => void;
   name: string;
+  tooltip?: string;
 }
 
-const SelectControlMultiple: React.FC<SelectControlMultiple> = ({ options, selection, setSelection, name }: SelectControlMultiple) => {
+const SelectControlMultiple: React.FC<SelectControlMultiple> = ({ options, selection, setSelection, name, tooltip = '' }: SelectControlMultiple) => {
   const handleChange = (event: SelectChangeEvent<string[]>) => {
     setSelection(event.target.value as string[]);
   };
 
-  return (
-    <MultiSelectContainer>
-      <FormControl variant="standard" fullWidth>
-        <InputLabel>{name}</InputLabel>
-        <Select
-          name={name}
-          value={selection}
-          multiple
-          onChange={handleChange}
-          input={<Input />}
-          renderValue={(selected) => {
-            const selectedArray = selected as string[];
-            if (selectedArray.length === 1) return selection[0];
-            if (selectedArray.length > 1) return 'Multiple selected';
-          }}
-          variant="standard"
-        >
-          {options.map((opt) => (
-            <MenuItem key={opt} value={opt}>
-              <Checkbox checked={selection.indexOf(opt) > -1} />
-              {opt}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </MultiSelectContainer>
-  );
+  if (tooltip !== '') {
+    return (
+      <Tooltip title={tooltip} arrow>
+        <MultiSelectContainer>
+          <FormControl variant="standard" fullWidth>
+            <InputLabel>{name}</InputLabel>
+            <Select
+              name={name}
+              value={selection}
+              multiple
+              onChange={handleChange}
+              input={<Input />}
+              renderValue={(selected) => {
+                const selectedArray = selected as string[];
+                if (selectedArray.length === 1) return selection[0];
+                if (selectedArray.length > 1) return 'Multiple selected';
+              }}
+              variant="standard"
+            >
+              {options.map((opt) => (
+                <MenuItem key={opt} value={opt}>
+                  <Checkbox checked={selection.indexOf(opt) > -1} />
+                  {opt}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </MultiSelectContainer>
+      </Tooltip>
+    );
+  } else
+    return (
+      <MultiSelectContainer>
+        <FormControl variant="standard" fullWidth>
+          <InputLabel>{name}</InputLabel>
+          <Select
+            name={name}
+            value={selection}
+            multiple
+            onChange={handleChange}
+            input={<Input />}
+            renderValue={(selected) => {
+              const selectedArray = selected as string[];
+              if (selectedArray.length === 1) return selection[0];
+              if (selectedArray.length > 1) return 'Multiple selected';
+            }}
+            variant="standard"
+          >
+            {options.map((opt) => (
+              <MenuItem key={opt} value={opt}>
+                <Checkbox checked={selection.indexOf(opt) > -1} />
+                {opt}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </MultiSelectContainer>
+    );
 };
 
 export default SelectControlMultiple;
