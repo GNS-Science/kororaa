2a83f1f283cc62a930bf64fba5c650dbe4bfb99e
chrisdicaprio <christopher.dicaprio@gmail.com>
2022-09-30 22:44:17 +1300

    link to GNS site for logo

--- src/views/disaggregations/constants/disaggregations.ts
+++
@@ -1,7 +0,0 @@
-export const locationTooltip = 'lorem ipsum locations';
-
-export const vs30Tooltip = 'lorem ipsum vs30';
-
-export const imtTooltip = 'lorem ipsum imt';
-
-export const poeTooltip = 'lorem ipsum poe';
