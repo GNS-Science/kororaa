a9ab3942c8259d3a207e67db86341e0625777e07
ben-chamberlain <ben.albrecht.chamberlain@gmail.com>
2022-09-30 17:21:57 +1300

    fix title spacing; remove styled grid;

--- src/views/about/AboutPage.tsx
+++ src/views/about/AboutPage.tsx
@@ -11,70 +11,65 @@ const PageContainer = styled('div')({
   marginBottom: '2rem',
 });
 
-const StyledGrid = styled(Grid)(() => ({
-  '& .MuiGrid-item': {
-    marginBottom: '0.5rem',
-  },
-}));
-
 const AboutPage: React.FC = () => {
   return (
     <PageContainer>
-      <StyledGrid container spacing={3} columns={{ sm: 6, md: 8, lg: 12 }}>
+      <Grid container columns={{ sm: 6, md: 8, lg: 12 }}>
         <Grid item xs={2} />
-        <Grid spacing={3} item xs={8}>
+        <Grid item xs={8}>
           <Typography variant="h2">About The NZ-NSHM</Typography>
+          <Grid container rowSpacing={1.5} direction="column">
+            <Grid item>
+              <Typography variant="body1">
+                <strong>
+                  The National Seismic Hazard Model calculates the likelihood and strength of earthquake shaking that may occur in different parts of Aotearoa New Zealand over specified time periods.
+                </strong>
+              </Typography>
+            </Grid>
 
-          <Grid item>
-            <Typography variant="body1">
-              <strong>
-                The National Seismic Hazard Model calculates the likelihood and strength of earthquake shaking that may occur in different parts of Aotearoa New Zealand over specified time periods.
-              </strong>
-            </Typography>
-          </Grid>
+            <Grid item>
+              <Typography variant="body1">GNS Science is the custodian of the Aotearoa New Zealand National Seismic Hazard Model (NSHM).</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1">
+                Background information on the NSHM Project as well as educational material can be found on the&nbsp;
+                <Link color="secondary" target="_blank" rel="noopener" href="https://www.gns.cri.nz/research-projects/national-seismic-hazard-model/">
+                  GNS Page
+                </Link>
+                . Looking for NSHM Results and Data? You&apos;re in the right place.
+              </Typography>
+            </Grid>
 
-          <Grid item>
-            <Typography variant="body1">GNS Science is the custodian of the Aotearoa New Zealand National Seismic Hazard Model (NSHM).</Typography>
-          </Grid>
-          <Grid item>
-            <Typography variant="body1">
-              Background information on the NSHM Project as well as educational material can be found on the&nbsp;
-              <Link color="secondary" target="_blank" rel="noopener" href="https://www.gns.cri.nz/research-projects/national-seismic-hazard-model/">
-                GNS Page
-              </Link>
-              . Looking for NSHM Results and Data? You&apos;re in the right place.
-            </Typography>
-          </Grid>
-
-          <Grid item>
-            <Typography variant="body1">
-              This site provides only seismic hazard results and is not to be used for engineering design. Please see MBIE for information related to engineering design and the NSHM.
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography variant="body1">The NZ NSHM provides forecasts of ground shaking for the next 100 years in New Zealand.</Typography>
-          </Grid>
-          <Grid item>
-            <Typography variant="body1">
-              Details on model development and implementation can be found in the&nbsp;
-              <Link underline="hover" component={RouterLink} color="secondary" to="/Resources/ScienceReports">
-                Science Reports
-              </Link>
-              &nbsp;page.
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography variant="body1">
-              Planned features for the site can be found in the&nbsp;
-              <Link underline="hover" component={RouterLink} color="secondary" to="/Previews">
-                Coming Features
-              </Link>
-              &nbsp;page.
-            </Typography>
+            <Grid item>
+              <Typography variant="body1">
+                This site provides only seismic hazard results and is not to be used for engineering design. Please see MBIE for information related to engineering design and the NSHM.
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1">The NZ NSHM provides forecasts of ground shaking for the next 100 years in New Zealand.</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1">
+                Details on model development and implementation can be found in the&nbsp;
+                <Link underline="hover" component={RouterLink} color="secondary" to="/Resources/ScienceReports">
+                  Science Reports
+                </Link>
+                &nbsp;page.
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1">
+                Planned features for the site can be found in the&nbsp;
+                <Link underline="hover" component={RouterLink} color="secondary" to="/Previews">
+                  Coming Features
+                </Link>
+                &nbsp;page.
+              </Typography>
+            </Grid>
           </Grid>
         </Grid>
         <Grid item xs={2} />
-      </StyledGrid>
+      </Grid>
     </PageContainer>
   );
 };
--- src/views/techinfo/TechInfoPage.tsx
+++ src/views/techinfo/TechInfoPage.tsx
@@ -17,12 +17,6 @@ const PageContainer = styled('div')({
   marginBottom: '2rem',
 });
 
-const StyledGrid = styled(Grid)(() => ({
-  '& .MuiGrid-item': {
-    marginBottom: '0.5rem',
-  },
-}));
-
 function createData(name: string, lat: number, lon: number) {
   return { name, lat, lon };
 }
@@ -68,144 +62,146 @@ const rows = [
 const TechInfoPage: React.FC = () => {
   return (
     <PageContainer>
-      <StyledGrid container spacing={4} direction="row" columns={{ sm: 6, md: 8, lg: 12 }}>
+      <Grid container columns={{ sm: 6, md: 8, lg: 12 }}>
         <Grid item xs={2} />
         <Grid item xs={8}>
           <Typography variant="h2">NZ-NSHM Technical Information</Typography>
-          <Grid item>
-            <Typography variant="body1">
-              <strong>
-                More detail on many of these concepts can be found in the&nbsp;
-                <Link underline="hover" component={RouterLink} color="secondary" to="/Resources/ScienceReports">
-                  Science Reports
+          <Grid container direction="column" rowSpacing={1.5}>
+            <Grid item>
+              <Typography variant="body1">
+                <strong>
+                  More detail on many of these concepts can be found in the&nbsp;
+                  <Link underline="hover" component={RouterLink} color="secondary" to="/Resources/ScienceReports">
+                    Science Reports
+                  </Link>
+                  &nbsp;page.
+                </strong>
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="h4">Orientation</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1">All results are provided in RotD50 orientation.</Typography>
+            </Grid>
+
+            <Grid item>
+              <Typography variant="h4">Forecast Timespan and Time-Dependence</Typography>
+            </Grid>
+
+            <Grid item>
+              <Typography variant="body1">
+                The NZ NSHM 2022 provides forecasts of ground shaking for the next 100 years. Time 100 year time dependence has been included in the model using time since the most recent known event
+                on faults (conditional probability of rupture) as well as increased seismicity rates in areas that have recently experienced earthquakes (e.g. Christchurch and Kaikoura). On average,
+                the time-dependence has increased the estimate of seismic hazard, though it does decrease it in some areas.
+              </Typography>
+            </Grid>
+
+            <Grid item>
+              <Typography variant="h4">Probability of Exceedance and Return Period</Typography>
+            </Grid>
+
+            <Grid item>
+              <Typography variant="body1">
+                <strong>Probability of Exceedance:</strong> The chance (or likelihood) that a certain level of ground shaking will be reached or exceeded over a certain time-interval. For example: a
+                PGA value of 0.82g for 10% PoE in 50 years states that there are 10% chances that this value of shaking will be reached or exceeded in the next 50 years.
+              </Typography>
+            </Grid>
+
+            <Grid item>
+              <Typography variant="body1">
+                <strong>Return period:</strong> associated with the recurrence interval of earthquakes and defined as the reciprocal of mean annual rates (i.e., number of events per year). Sometimes,
+                In terms of hazard results, the reciprocal of annual rates of exceedances is also termed as return period. However, it should be noted that the return period associated with exceedance
+                of a certain level of ground shaking does not imply a periodicity in ground shaking exceedance.
+              </Typography>
+            </Grid>
+
+            <Grid item>
+              <Typography variant="body1">
+                <strong>
+                  However, it should be noted that the return period associated with exceedance of a certain level of ground shaking does not imply a periodicity in ground shaking exceedance.
+                </strong>
+              </Typography>
+            </Grid>
+
+            <Grid item>
+              <Typography variant="h4">Uncertainty Bounds</Typography>
+            </Grid>
+
+            <Grid item>
+              <Typography variant="body1">
+                A key feature of the National Seismic Hazard Model revision is the inclusion of epistemic uncertainty, which is expressed as uncertainty bounds. Uncertainty bounds reflect our
+                confidence (i.e. the likelihood) that the true hazard lies within those limits. For example, we estimate there is an 80% chance the true hazard lies within the 80% confidence bounds.
+              </Typography>
+            </Grid>
+
+            <Grid item>
+              <Typography variant="body1">
+                Logic trees were developed for key SRM and GMCM epistemic uncertainties. These are uncertainties related to our knowledge of earthquake occurrence that we are not directly able to
+                constrain by existing data. Weights were assigned to the logic tree branches as a degree of belief based on scientific judgement from experts in the respective fields.
+              </Typography>
+            </Grid>
+
+            <Grid item>
+              <Typography variant="body1">
+                By modelling the full logic tree, we are able to provide the mean hazard estimate with estimated confidence bounds. The logic tree branches and confidence bounds can be considered to
+                explore credible and alternate versions of the next 100 years.
+              </Typography>
+            </Grid>
+
+            <Grid item>
+              <Typography variant="body1">
+                On the&nbsp;
+                <Link underline="hover" component={RouterLink} color="secondary" to="/HazardMaps">
+                  Hazard Maps
                 </Link>
-                &nbsp;page.
-              </strong>
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography variant="h4">Orientation</Typography>
-          </Grid>
-          <Grid item>
-            <Typography variant="body1">All results are provided in RotD50 orientation.</Typography>
-          </Grid>
+                &nbsp;page it is possible to select Coefficient of Variation (CoV) as one of the &quot;Statistic&quot; options. The CoV is the standard deviation of the annual probability of
+                exceedance divided by the probability chosen (e.g. 10% in 50 years).
+              </Typography>
+            </Grid>
 
-          <Grid item>
-            <Typography variant="h4">Forecast Timespan and Time-Dependence</Typography>
-          </Grid>
+            <Grid item>
+              <Typography variant="h4">Calculation Grid and Location List</Typography>
+            </Grid>
 
-          <Grid item>
-            <Typography variant="body1">
-              The NZ NSHM 2022 provides forecasts of ground shaking for the next 100 years. Time 100 year time dependence has been included in the model using time since the most recent known event on
-              faults (conditional probability of rupture) as well as increased seismicity rates in areas that have recently experienced earthquakes (e.g. Christchurch and Kaikoura). On average, the
-              time-dependence has increased the estimate of seismic hazard, though it does decrease it in some areas.
-            </Typography>
-          </Grid>
+            <Grid item>
+              <Typography variant="body1">
+                The NZ-NSHM has been calculated on a 0.1 deg grid (approximately 11km resolution). In addition, hazard curves and spectra are available for a number of population centres listed under
+                “Locations” on the Site Hazard Note that the user must still specify site conditions (Vs30) for these locations as they are not pre-set.
+              </Typography>
+            </Grid>
 
-          <Grid item>
-            <Typography variant="h4">Probability of Exceedance and Return Period</Typography>
-          </Grid>
-
-          <Grid item>
-            <Typography variant="body1">
-              <strong>Probability of Exceedance:</strong> The chance (or likelihood) that a certain level of ground shaking will be reached or exceeded over a certain time-interval. For example: a PGA
-              value of 0.82g for 10% PoE in 50 years states that there are 10% chances that this value of shaking will be reached or exceeded in the next 50 years.
-            </Typography>
-          </Grid>
-
-          <Grid item>
-            <Typography variant="body1">
-              <strong>Return period:</strong> associated with the recurrence interval of earthquakes and defined as the reciprocal of mean annual rates (i.e., number of events per year). Sometimes, In
-              terms of hazard results, the reciprocal of annual rates of exceedances is also termed as return period. However, it should be noted that the return period associated with exceedance of a
-              certain level of ground shaking does not imply a periodicity in ground shaking exceedance.
-            </Typography>
-          </Grid>
-
-          <Grid item>
-            <Typography variant="body1">
-              <strong>
-                However, it should be noted that the return period associated with exceedance of a certain level of ground shaking does not imply a periodicity in ground shaking exceedance.
-              </strong>
-            </Typography>
-          </Grid>
-
-          <Grid item>
-            <Typography variant="h4">Uncertainty Bounds</Typography>
-          </Grid>
-
-          <Grid item>
-            <Typography variant="body1">
-              A key feature of the National Seismic Hazard Model revision is the inclusion of epistemic uncertainty, which is expressed as uncertainty bounds. Uncertainty bounds reflect our confidence
-              (i.e. the likelihood) that the true hazard lies within those limits. For example, we estimate there is an 80% chance the true hazard lies within the 80% confidence bounds.
-            </Typography>
-          </Grid>
-
-          <Grid item>
-            <Typography variant="body1">
-              Logic trees were developed for key SRM and GMCM epistemic uncertainties. These are uncertainties related to our knowledge of earthquake occurrence that we are not directly able to
-              constrain by existing data. Weights were assigned to the logic tree branches as a degree of belief based on scientific judgement from experts in the respective fields.
-            </Typography>
-          </Grid>
-
-          <Grid item>
-            <Typography variant="body1">
-              By modelling the full logic tree, we are able to provide the mean hazard estimate with estimated confidence bounds. The logic tree branches and confidence bounds can be considered to
-              explore credible and alternate versions of the next 100 years.
-            </Typography>
-          </Grid>
-
-          <Grid item>
-            <Typography variant="body1">
-              On the&nbsp;
-              <Link underline="hover" component={RouterLink} color="secondary" to="/HazardMaps">
-                Hazard Maps
-              </Link>
-              &nbsp;page it is possible to select Coefficient of Variation (CoV) as one of the &quot;Statistic&quot; options. The CoV is the standard deviation of the annual probability of exceedance
-              divided by the probability chosen (e.g. 10% in 50 years).
-            </Typography>
-          </Grid>
-
-          <Grid item>
-            <Typography variant="h4">Calculation Grid and Location List</Typography>
-          </Grid>
-
-          <Grid item>
-            <Typography variant="body1">
-              The NZ-NSHM has been calculated on a 0.1 deg grid (approximately 11km resolution). In addition, hazard curves and spectra are available for a number of population centres listed under
-              “Locations” on the Site Hazard Note that the user must still specify site conditions (Vs30) for these locations as they are not pre-set.
-            </Typography>
-          </Grid>
-
-          <Grid item>
-            <Typography variant="body1">The specific coordinates for these locations are:</Typography>
-          </Grid>
-          <Grid item>
-            <TableContainer>
-              <Table sx={{ width: '80%', marginLeft: 'auto', marginRight: 'auto' }} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Location</TableCell>
-                    <TableCell align="right">lat</TableCell>
-                    <TableCell align="right">lon</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {rows.map((row) => (
-                    <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                      <TableCell component="th" scope="row">
-                        {row.name}
-                      </TableCell>
-                      <TableCell align="right">{row.lat}</TableCell>
-                      <TableCell align="right">{row.lon}</TableCell>
+            <Grid item>
+              <Typography variant="body1">The specific coordinates for these locations are:</Typography>
+            </Grid>
+            <Grid item>
+              <TableContainer>
+                <Table sx={{ width: '80%', marginLeft: 'auto', marginRight: 'auto' }} aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Location</TableCell>
+                      <TableCell align="right">lat</TableCell>
+                      <TableCell align="right">lon</TableCell>
                     </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
+                  </TableHead>
+                  <TableBody>
+                    {rows.map((row) => (
+                      <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                        <TableCell component="th" scope="row">
+                          {row.name}
+                        </TableCell>
+                        <TableCell align="right">{row.lat}</TableCell>
+                        <TableCell align="right">{row.lon}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Grid>
           </Grid>
+          <Grid item xs={2} />
         </Grid>
-        <Grid item xs={2} />
-      </StyledGrid>
+      </Grid>
     </PageContainer>
   );
 };
