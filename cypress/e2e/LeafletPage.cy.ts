91a09c0f8e5a636427541d40e3fd302c654bc47a
ben-chamberlain <ben.albrecht.chamberlain@gmail.com>
2022-10-11 13:46:06 +1300

    fix hazmap tests;

--- cypress/e2e/LeafletPage.cy.ts
+++ cypress/e2e/LeafletPage.cy.ts
@@ -8,10 +8,6 @@ describe('HazardMapsPage', () => {
     cy.get('[id="leaflet-map-container"]').should('exist');
   });
 
-  it('Map displays geojson data', () => {
-    cy.get('[class="leaflet-interactive"]').should('have.length', 241);
-  });
-
   it('Map initially displays default map tile layer and changes map tile layer when user selects different option', () => {
     cy.get('img[src*="arcgisonline"]');
     cy.get('[class="leaflet-control-layers-toggle"]').focus();
@@ -21,22 +17,22 @@ describe('HazardMapsPage', () => {
 
   it('Renders drawer button and opens the drawer when clicked, drawer close button closes drawer', () => {
     cy.get('[data-testid="ChevronRightIcon"]').click();
-    cy.get('[class="MuiPaper-root MuiPaper-elevation MuiPaper-elevation0 MuiDrawer-paper MuiDrawer-paperAnchorLeft MuiDrawer-paperAnchorDockedLeft css-p1ulq7-MuiPaper-root-MuiDrawer-paper"]').should(
-      'be.visible',
-    );
-    cy.get('[data-testid="ChevronLeftIcon"]').click();
-    cy.get('[class="MuiPaper-root MuiPaper-elevation MuiPaper-elevation0 MuiDrawer-paper MuiDrawer-paperAnchorLeft MuiDrawer-paperAnchorDockedLeft css-p1ulq7-MuiPaper-root-MuiDrawer-paper"]').should(
-      'not.be.visible',
-    );
+    cy.get('h4').contains('Hazard Maps').should('be.visible');
+    cy.get('[data-testid="ChevronLeftIcon"]').click({ force: true });
+    cy.get('h4').contains('Hazard Maps').should('not.be.visible');
   });
 
   it('When fullscreen button is pressed, the map becomes full screen', () => {
-    cy.get('a[title="Full Screen"]').click()
-    cy.get('[id="leaflet-map-container"]').should('have.class', 'leaflet-pseudo-fullscreen')
-  })
+    cy.get('a[title="Full Screen"]').click();
+    cy.get('[id="leaflet-map-container"]').should('have.class', 'leaflet-pseudo-fullscreen');
+  });
 
   it('When fullscreen button is pressed and map is full screen, the map becomes smaller', () => {
-    cy.get('a[title="Exit Full Screen"]').click()
-    cy.get('[id="leaflet-map-container"]').should('not.have.class', 'leaflet-pseudo-fullscreen')
-  })
+    cy.get('a[title="Exit Full Screen"]').click();
+    cy.get('[id="leaflet-map-container"]').should('not.have.class', 'leaflet-pseudo-fullscreen');
+  });
+
+  it('Map displays geojson data', () => {
+    cy.get('[class="leaflet-interactive"]').should('have.length', 3478);
+  });
 });
