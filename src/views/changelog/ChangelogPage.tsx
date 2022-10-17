5c1141c22a49fa0aa670f4cbdf162396f9ab5612
ben-chamberlain <ben.albrecht.chamberlain@gmail.com>
2022-10-18 11:07:55 +1300

    changelog page styling;

--- src/views/changelog/ChangelogPage.tsx
+++ src/views/changelog/ChangelogPage.tsx
@@ -1,14 +1,51 @@
-import React from 'react';
-import { Typography } from '@mui/material';
-import * as changelog from '../../../CHANGELOG.md';
+import React, { useState, useEffect } from 'react';
+import { Typography, Grid } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import CHANGELOG from '../../CHANGELOG.md';
 import ReactMarkdown from 'react-markdown';
 
+const PageContainer = styled('div')({
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '2rem',
+  marginBottom: '2rem',
+});
+
+const TitleContainer = styled('div')({
+  justifyContent: 'left',
+  textAlign: 'left',
+  width: '100%',
+  paddingBottom: '1rem',
+});
+
 const ChangelogPage: React.FC = () => {
+  const [markdown, setMarkdown] = useState('');
+
+  useEffect(() => {
+    fetch(CHANGELOG)
+      .then((res) => res.text())
+      .then((text) => setMarkdown(text));
+  }, []);
+
   return (
-    <div>
-      <Typography variant="h2">Changelog</Typography>
-      <ReactMarkdown>{changelog}</ReactMarkdown>
-    </div>
+    <PageContainer>
+      <Grid container columns={{ sm: 6, md: 8, lg: 12 }}>
+        <Grid item xs={2} />
+        <Grid item xs={8}>
+          <Grid container spacing={2} columns={{ sm: 6, md: 8, lg: 12 }}>
+            <Grid item xs={12}>
+              <TitleContainer>
+                <Typography variant="h2">Releases</Typography>
+              </TitleContainer>
+            </Grid>
+            <Grid item>
+              <ReactMarkdown>{markdown}</ReactMarkdown>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={2} />
+      </Grid>
+    </PageContainer>
   );
 };
 
