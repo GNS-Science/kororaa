eac145c2f115d137c62c952cd9e074d17c7bf1fb
ben-chamberlain <ben.albrecht.chamberlain@gmail.com>
2023-03-03 15:45:35 +1300

    animated rupture uses new dataset; update toshi nest to 3.7.6; (#328)

--- src/views/ruptureAnimation/RuptureAnimationPage.tsx
+++ src/views/ruptureAnimation/RuptureAnimationPage.tsx
@@ -2,24 +2,21 @@ import React, { useState } from 'react';
 import { LeafletMap } from '@gns-science/toshi-nest';
 import '../../css/leaflet.timedimension.control.css';
 
-import timeDimensionData from '../../mocks/mockData/puysegur_rupture_surfaces_above_2e-4.json';
-import timeDimensionUnderlay from '../../mocks/mockData/surfaces_puysegur.json';
+import timeDimensionData from '../../mocks/mockData/kaikoura_rupture_surfaces_above_2e-4.json';
+import timeDimensionUnderlay from '../../mocks/mockData/CRU_fault_surfaces.json';
 
 export const RuptureAnimationPage: React.FC = () => {
   const [zoomLevel, setZoomLevel] = useState<number>(5);
   const [fullscreen, setFullscreen] = React.useState<boolean>(false);
   const zoom = 5;
   const nzCentre = [-40.946, 174.167];
+  const timeArray = Array(Object.values(timeDimensionData).length)
+    .fill(0)
+    .map((_, i) => i + 1);
 
-  const currentTime = new Date();
-  currentTime.setUTCDate(1);
   const timeDimensionOptions = {
-    currentTime: 3,
-    times: [
-      3, 4, 5, 6, 7, 8, 12, 14, 21, 23, 251, 412, 413, 414, 419, 420, 530, 531, 533, 2714, 3832, 3842, 4582, 4585, 5624, 5727, 6890, 8772, 9323, 9436, 9943, 9975, 10018, 10031, 10078, 10080, 10081,
-      10655, 10708, 11286, 11299, 11300, 11850, 11861, 11865, 11870, 12233, 12381, 12389, 12390, 12779, 13153, 13832, 13834, 14130, 14372, 14806, 14980, 15131, 15134, 15259, 15371, 15375, 15395,
-      15470, 15528, 15548, 15556, 15615, 15622, 15668, 15685, 15711, 15719, 15735, 15746, 15758, 15766, 15767, 15779, 15785, 15790,
-    ],
+    currentTime: 1,
+    times: timeArray,
   };
   const timeDimensionControlOptions = {
     loopButton: true,
