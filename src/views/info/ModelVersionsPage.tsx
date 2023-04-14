2fc6993918c98f66ca18f1df621ee676ca28c71e
Chris B Chamberlain <chrisbc@artisan.co.nz>
2023-04-14 12:37:09 +1200

    Update ModelVersionsPage.tsx
    remove redundant useMemo import

--- src/views/info/ModelVersionsPage.tsx
+++ src/views/info/ModelVersionsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import ReactMarkdown from 'react-markdown';
