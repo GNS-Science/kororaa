163fbe0e029f429284ff563e8c265d0f09986414
voj <o.jurgens@gns.cri.nz>
2025-11-27 15:29:51 +1300

    WIP

--- src/views/hazardCharts/__generated__/HazardChartsPlotsViewQuery.graphql.ts
+++ src/views/hazardCharts/__generated__/HazardChartsPlotsViewQuery.graphql.ts
@@ -8,7 +8,7 @@
 /* eslint-disable */
 // @ts-nocheck
 
-import { ConcreteRequest } from "relay-runtime";
+import { ConcreteRequest } from 'relay-runtime';
 export type HazardChartsPlotsViewQuery$variables = {
   aggs?: ReadonlyArray<string | null | undefined> | null | undefined;
   hazard_model?: string | null | undefined;
@@ -18,286 +18,280 @@ export type HazardChartsPlotsViewQuery$variables = {
   vs30s?: ReadonlyArray<number | null | undefined> | null | undefined;
 };
 export type HazardChartsPlotsViewQuery$data = {
-  readonly hazard_curves:
-    | {
-        readonly curves:
-          | ReadonlyArray<
-              | {
-                  readonly agg: string | null | undefined;
-                  readonly curve:
-                    | {
-                        readonly levels: ReadonlyArray<number | null | undefined> | null | undefined;
-                        readonly values: ReadonlyArray<number | null | undefined> | null | undefined;
-                      }
-                    | null
-                    | undefined;
-                  readonly hazard_model: string | null | undefined;
-                  readonly imt: string | null | undefined;
-                  readonly loc: string | null | undefined;
-                  readonly vs30: number | null | undefined;
-                }
-              | null
-              | undefined
-            >
-          | null
-          | undefined;
-        readonly locations:
-          | ReadonlyArray<
-              | {
-                  readonly code: string | null | undefined;
-                  readonly key: string | null | undefined;
-                  readonly lat: number | null | undefined;
-                  readonly lon: number | null | undefined;
-                  readonly name: string | null | undefined;
-                  readonly resolution: number | null | undefined;
-                }
-              | null
-              | undefined
-            >
-          | null
-          | undefined;
-        readonly ok: boolean | null | undefined;
-      }
-    | null
-    | undefined;
+  readonly hazard_curves: {
+    readonly curves: ReadonlyArray<{
+      readonly agg: string | null | undefined;
+      readonly curve: {
+        readonly levels: ReadonlyArray<number | null | undefined> | null | undefined;
+        readonly values: ReadonlyArray<number | null | undefined> | null | undefined;
+      } | null | undefined;
+      readonly hazard_model: string | null | undefined;
+      readonly imt: string | null | undefined;
+      readonly loc: string | null | undefined;
+      readonly vs30: number | null | undefined;
+    } | null | undefined> | null | undefined;
+    readonly locations: ReadonlyArray<{
+      readonly code: string | null | undefined;
+      readonly key: string | null | undefined;
+      readonly lat: number | null | undefined;
+      readonly lon: number | null | undefined;
+      readonly name: string | null | undefined;
+      readonly resolution: number | null | undefined;
+    } | null | undefined> | null | undefined;
+    readonly ok: boolean | null | undefined;
+  } | null | undefined;
 };
 export type HazardChartsPlotsViewQuery = {
   response: HazardChartsPlotsViewQuery$data;
   variables: HazardChartsPlotsViewQuery$variables;
 };
 
-const node: ConcreteRequest = (function () {
-  var v0 = {
-      defaultValue: null,
-      kind: "LocalArgument",
-      name: "aggs",
-    },
-    v1 = {
-      defaultValue: null,
-      kind: "LocalArgument",
-      name: "hazard_model",
-    },
-    v2 = {
-      defaultValue: null,
-      kind: "LocalArgument",
-      name: "imts",
-    },
-    v3 = {
-      defaultValue: null,
-      kind: "LocalArgument",
-      name: "locs",
-    },
-    v4 = {
-      defaultValue: null,
-      kind: "LocalArgument",
-      name: "resolution",
-    },
-    v5 = {
-      defaultValue: null,
-      kind: "LocalArgument",
-      name: "vs30s",
-    },
-    v6 = [
+const node: ConcreteRequest = (function(){
+var v0 = {
+  "defaultValue": null,
+  "kind": "LocalArgument",
+  "name": "aggs"
+},
+v1 = {
+  "defaultValue": null,
+  "kind": "LocalArgument",
+  "name": "hazard_model"
+},
+v2 = {
+  "defaultValue": null,
+  "kind": "LocalArgument",
+  "name": "imts"
+},
+v3 = {
+  "defaultValue": null,
+  "kind": "LocalArgument",
+  "name": "locs"
+},
+v4 = {
+  "defaultValue": null,
+  "kind": "LocalArgument",
+  "name": "resolution"
+},
+v5 = {
+  "defaultValue": null,
+  "kind": "LocalArgument",
+  "name": "vs30s"
+},
+v6 = [
+  {
+    "alias": "hazard_curves",
+    "args": [
       {
-        alias: "hazard_curves",
-        args: [
-          {
-            kind: "Variable",
-            name: "aggs",
-            variableName: "aggs",
-          },
-          {
-            kind: "Variable",
-            name: "hazard_model",
-            variableName: "hazard_model",
-          },
-          {
-            kind: "Variable",
-            name: "imts",
-            variableName: "imts",
-          },
-          {
-            kind: "Variable",
-            name: "locs",
-            variableName: "locs",
-          },
-          {
-            kind: "Variable",
-            name: "resolution",
-            variableName: "resolution",
-          },
-          {
-            kind: "Variable",
-            name: "vs30s",
-            variableName: "vs30s",
-          },
-        ],
-        concreteType: "ToshiHazardCurveResult",
-        kind: "LinkedField",
-        name: "HAZARD_hazard_curves",
-        plural: false,
-        selections: [
-          {
-            alias: null,
-            args: null,
-            kind: "ScalarField",
-            name: "ok",
-            storageKey: null,
-          },
-          {
-            alias: null,
-            args: null,
-            concreteType: "GriddedLocation",
-            kind: "LinkedField",
-            name: "locations",
-            plural: true,
-            selections: [
-              {
-                alias: null,
-                args: null,
-                kind: "ScalarField",
-                name: "lat",
-                storageKey: null,
-              },
-              {
-                alias: null,
-                args: null,
-                kind: "ScalarField",
-                name: "lon",
-                storageKey: null,
-              },
-              {
-                alias: null,
-                args: null,
-                kind: "ScalarField",
-                name: "resolution",
-                storageKey: null,
-              },
-              {
-                alias: null,
-                args: null,
-                kind: "ScalarField",
-                name: "code",
-                storageKey: null,
-              },
-              {
-                alias: null,
-                args: null,
-                kind: "ScalarField",
-                name: "name",
-                storageKey: null,
-              },
-              {
-                alias: null,
-                args: null,
-                kind: "ScalarField",
-                name: "key",
-                storageKey: null,
-              },
-            ],
-            storageKey: null,
-          },
-          {
-            alias: null,
-            args: null,
-            concreteType: "ToshiHazardResult",
-            kind: "LinkedField",
-            name: "curves",
-            plural: true,
-            selections: [
-              {
-                alias: null,
-                args: null,
-                kind: "ScalarField",
-                name: "hazard_model",
-                storageKey: null,
-              },
-              {
-                alias: null,
-                args: null,
-                kind: "ScalarField",
-                name: "imt",
-                storageKey: null,
-              },
-              {
-                alias: null,
-                args: null,
-                kind: "ScalarField",
-                name: "loc",
-                storageKey: null,
-              },
-              {
-                alias: null,
-                args: null,
-                kind: "ScalarField",
-                name: "agg",
-                storageKey: null,
-              },
-              {
-                alias: null,
-                args: null,
-                kind: "ScalarField",
-                name: "vs30",
-                storageKey: null,
-              },
-              {
-                alias: null,
-                args: null,
-                concreteType: "ToshiHazardCurve",
-                kind: "LinkedField",
-                name: "curve",
-                plural: false,
-                selections: [
-                  {
-                    alias: null,
-                    args: null,
-                    kind: "ScalarField",
-                    name: "levels",
-                    storageKey: null,
-                  },
-                  {
-                    alias: null,
-                    args: null,
-                    kind: "ScalarField",
-                    name: "values",
-                    storageKey: null,
-                  },
-                ],
-                storageKey: null,
-              },
-            ],
-            storageKey: null,
-          },
-        ],
-        storageKey: null,
+        "kind": "Variable",
+        "name": "aggs",
+        "variableName": "aggs"
       },
-    ];
-  return {
-    fragment: {
-      argumentDefinitions: [v0 /*: any*/, v1 /*: any*/, v2 /*: any*/, v3 /*: any*/, v4 /*: any*/, v5 /*: any*/],
-      kind: "Fragment",
-      metadata: null,
-      name: "HazardChartsPlotsViewQuery",
-      selections: v6 /*: any*/,
-      type: "Query",
-      abstractKey: null,
-    },
-    kind: "Request",
-    operation: {
-      argumentDefinitions: [v1 /*: any*/, v5 /*: any*/, v2 /*: any*/, v3 /*: any*/, v0 /*: any*/, v4 /*: any*/],
-      kind: "Operation",
-      name: "HazardChartsPlotsViewQuery",
-      selections: v6 /*: any*/,
-    },
-    params: {
-      cacheID: "abc3434508bddc1a158cce78bfbeda81",
-      id: null,
-      metadata: {},
-      name: "HazardChartsPlotsViewQuery",
-      operationKind: "query",
-      text: "query HazardChartsPlotsViewQuery(\n  $hazard_model: String\n  $vs30s: [Int]\n  $imts: [String]\n  $locs: [String]\n  $aggs: [String]\n  $resolution: Float\n) {\n  hazard_curves: HAZARD_hazard_curves(hazard_model: $hazard_model, vs30s: $vs30s, imts: $imts, locs: $locs, aggs: $aggs, resolution: $resolution) {\n    ok\n    locations {\n      lat\n      lon\n      resolution\n      code\n      name\n      key\n    }\n    curves {\n      hazard_model\n      imt\n      loc\n      agg\n      vs30\n      curve {\n        levels\n        values\n      }\n    }\n  }\n}\n",
-    },
-  };
+      {
+        "kind": "Variable",
+        "name": "hazard_model",
+        "variableName": "hazard_model"
+      },
+      {
+        "kind": "Variable",
+        "name": "imts",
+        "variableName": "imts"
+      },
+      {
+        "kind": "Variable",
+        "name": "locs",
+        "variableName": "locs"
+      },
+      {
+        "kind": "Variable",
+        "name": "resolution",
+        "variableName": "resolution"
+      },
+      {
+        "kind": "Variable",
+        "name": "vs30s",
+        "variableName": "vs30s"
+      }
+    ],
+    "concreteType": "ToshiHazardCurveResult",
+    "kind": "LinkedField",
+    "name": "HAZARD_hazard_curves",
+    "plural": false,
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "kind": "ScalarField",
+        "name": "ok",
+        "storageKey": null
+      },
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "GriddedLocation",
+        "kind": "LinkedField",
+        "name": "locations",
+        "plural": true,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "lat",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "lon",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "resolution",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "code",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "name",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "key",
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      },
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "ToshiHazardResult",
+        "kind": "LinkedField",
+        "name": "curves",
+        "plural": true,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "hazard_model",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "imt",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "loc",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "agg",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "vs30",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "ToshiHazardCurve",
+            "kind": "LinkedField",
+            "name": "curve",
+            "plural": false,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "kind": "ScalarField",
+                "name": "levels",
+                "storageKey": null
+              },
+              {
+                "alias": null,
+                "args": null,
+                "kind": "ScalarField",
+                "name": "values",
+                "storageKey": null
+              }
+            ],
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "storageKey": null
+  }
+];
+return {
+  "fragment": {
+    "argumentDefinitions": [
+      (v0/*: any*/),
+      (v1/*: any*/),
+      (v2/*: any*/),
+      (v3/*: any*/),
+      (v4/*: any*/),
+      (v5/*: any*/)
+    ],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "HazardChartsPlotsViewQuery",
+    "selections": (v6/*: any*/),
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [
+      (v1/*: any*/),
+      (v5/*: any*/),
+      (v2/*: any*/),
+      (v3/*: any*/),
+      (v0/*: any*/),
+      (v4/*: any*/)
+    ],
+    "kind": "Operation",
+    "name": "HazardChartsPlotsViewQuery",
+    "selections": (v6/*: any*/)
+  },
+  "params": {
+    "cacheID": "abc3434508bddc1a158cce78bfbeda81",
+    "id": null,
+    "metadata": {},
+    "name": "HazardChartsPlotsViewQuery",
+    "operationKind": "query",
+    "text": "query HazardChartsPlotsViewQuery(\n  $hazard_model: String\n  $vs30s: [Int]\n  $imts: [String]\n  $locs: [String]\n  $aggs: [String]\n  $resolution: Float\n) {\n  hazard_curves: HAZARD_hazard_curves(hazard_model: $hazard_model, vs30s: $vs30s, imts: $imts, locs: $locs, aggs: $aggs, resolution: $resolution) {\n    ok\n    locations {\n      lat\n      lon\n      resolution\n      code\n      name\n      key\n    }\n    curves {\n      hazard_model\n      imt\n      loc\n      agg\n      vs30\n      curve {\n        levels\n        values\n      }\n    }\n  }\n}\n"
+  }
+};
 })();
 
 (node as any).hash = "2859b7096e8f3f3f3259c33fb4acc13d";
--- src/views/info/__generated__/ScienceReportsPageQuery.graphql.ts
+++ src/views/info/__generated__/ScienceReportsPageQuery.graphql.ts
@@ -8,169 +8,156 @@
 /* eslint-disable */
 // @ts-nocheck
 
-import { ConcreteRequest } from "relay-runtime";
+import { ConcreteRequest } from 'relay-runtime';
 export type ReportStatusEnum = "Published" | "Review" | "Undefined" | "%future added value";
 export type ScienceReportsPageQuery$variables = Record<PropertyKey, never>;
 export type ScienceReportsPageQuery$data = {
-  readonly science_reports:
-    | {
-        readonly ok: boolean | null | undefined;
-        readonly reports:
-          | ReadonlyArray<
-              | {
-                  readonly bibliographic_ref: string | null | undefined;
-                  readonly filename: string | null | undefined;
-                  readonly lead_author:
-                    | {
-                        readonly name: string | null | undefined;
-                      }
-                    | null
-                    | undefined;
-                  readonly notes: string | null | undefined;
-                  readonly publication_date: any | null | undefined;
-                  readonly report_number: string | null | undefined;
-                  readonly status: ReportStatusEnum | null | undefined;
-                  readonly title: string | null | undefined;
-                }
-              | null
-              | undefined
-            >
-          | null
-          | undefined;
-      }
-    | null
-    | undefined;
+  readonly science_reports: {
+    readonly ok: boolean | null | undefined;
+    readonly reports: ReadonlyArray<{
+      readonly bibliographic_ref: string | null | undefined;
+      readonly filename: string | null | undefined;
+      readonly lead_author: {
+        readonly name: string | null | undefined;
+      } | null | undefined;
+      readonly notes: string | null | undefined;
+      readonly publication_date: any | null | undefined;
+      readonly report_number: string | null | undefined;
+      readonly status: ReportStatusEnum | null | undefined;
+      readonly title: string | null | undefined;
+    } | null | undefined> | null | undefined;
+  } | null | undefined;
 };
 export type ScienceReportsPageQuery = {
   response: ScienceReportsPageQuery$data;
   variables: ScienceReportsPageQuery$variables;
 };
 
-const node: ConcreteRequest = (function () {
-  var v0 = [
-    {
-      alias: "science_reports",
-      args: null,
-      concreteType: "ScienceReportResult",
-      kind: "LinkedField",
-      name: "KORORAA_science_reports",
-      plural: false,
-      selections: [
-        {
-          alias: null,
-          args: null,
-          kind: "ScalarField",
-          name: "ok",
-          storageKey: null,
-        },
-        {
-          alias: null,
-          args: null,
-          concreteType: "ScienceReport",
-          kind: "LinkedField",
-          name: "reports",
-          plural: true,
-          selections: [
-            {
-              alias: null,
-              args: null,
-              kind: "ScalarField",
-              name: "filename",
-              storageKey: null,
-            },
-            {
-              alias: null,
-              args: null,
-              kind: "ScalarField",
-              name: "title",
-              storageKey: null,
-            },
-            {
-              alias: null,
-              args: null,
-              kind: "ScalarField",
-              name: "status",
-              storageKey: null,
-            },
-            {
-              alias: null,
-              args: null,
-              kind: "ScalarField",
-              name: "notes",
-              storageKey: null,
-            },
-            {
-              alias: null,
-              args: null,
-              kind: "ScalarField",
-              name: "report_number",
-              storageKey: null,
-            },
-            {
-              alias: null,
-              args: null,
-              concreteType: "Person",
-              kind: "LinkedField",
-              name: "lead_author",
-              plural: false,
-              selections: [
-                {
-                  alias: null,
-                  args: null,
-                  kind: "ScalarField",
-                  name: "name",
-                  storageKey: null,
-                },
-              ],
-              storageKey: null,
-            },
-            {
-              alias: null,
-              args: null,
-              kind: "ScalarField",
-              name: "publication_date",
-              storageKey: null,
-            },
-            {
-              alias: null,
-              args: null,
-              kind: "ScalarField",
-              name: "bibliographic_ref",
-              storageKey: null,
-            },
-          ],
-          storageKey: null,
-        },
-      ],
-      storageKey: null,
-    },
-  ];
-  return {
-    fragment: {
-      argumentDefinitions: [],
-      kind: "Fragment",
-      metadata: null,
-      name: "ScienceReportsPageQuery",
-      selections: v0 /*: any*/,
-      type: "Query",
-      abstractKey: null,
-    },
-    kind: "Request",
-    operation: {
-      argumentDefinitions: [],
-      kind: "Operation",
-      name: "ScienceReportsPageQuery",
-      selections: v0 /*: any*/,
-    },
-    params: {
-      cacheID: "35abbaf27ff7e061b08e5067455a99d6",
-      id: null,
-      metadata: {},
-      name: "ScienceReportsPageQuery",
-      operationKind: "query",
-      text: "query ScienceReportsPageQuery {\n  science_reports: KORORAA_science_reports {\n    ok\n    reports {\n      filename\n      title\n      status\n      notes\n      report_number\n      lead_author {\n        name\n      }\n      publication_date\n      bibliographic_ref\n    }\n  }\n}\n",
-    },
-  };
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "alias": "science_reports",
+    "args": null,
+    "concreteType": "ScienceReportResult",
+    "kind": "LinkedField",
+    "name": "KORORAA_science_reports",
+    "plural": false,
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "kind": "ScalarField",
+        "name": "ok",
+        "storageKey": null
+      },
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "ScienceReport",
+        "kind": "LinkedField",
+        "name": "reports",
+        "plural": true,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "filename",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "title",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "status",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "notes",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "report_number",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "Person",
+            "kind": "LinkedField",
+            "name": "lead_author",
+            "plural": false,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "kind": "ScalarField",
+                "name": "name",
+                "storageKey": null
+              }
+            ],
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "publication_date",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "bibliographic_ref",
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "storageKey": null
+  }
+];
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "ScienceReportsPageQuery",
+    "selections": (v0/*: any*/),
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "ScienceReportsPageQuery",
+    "selections": (v0/*: any*/)
+  },
+  "params": {
+    "cacheID": "35abbaf27ff7e061b08e5067455a99d6",
+    "id": null,
+    "metadata": {},
+    "name": "ScienceReportsPageQuery",
+    "operationKind": "query",
+    "text": "query ScienceReportsPageQuery {\n  science_reports: KORORAA_science_reports {\n    ok\n    reports {\n      filename\n      title\n      status\n      notes\n      report_number\n      lead_author {\n        name\n      }\n      publication_date\n      bibliographic_ref\n    }\n  }\n}\n"
+  }
+};
 })();
 
 (node as any).hash = "ed0ecf7189d6af3b7678b2055f36fed6";
