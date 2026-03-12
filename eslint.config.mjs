e207d9573d9ec7ac626efe3a186662909216774f
chrisdicaprio <christopher.dicaprio@gmail.com>
2026-03-13 08:36:40 +1300

    ignore CLAUDE.md

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,7 @@ const compat = new FlatCompat({
 
 export default [
   {
-    ignores: ["**/dist", "build/**", "public/**", "CLAUDE.md"],
+    ignores: ["**/dist", "build/**", "public/**"],
   },
   ...fixupConfigRules(
     compat.extends(
