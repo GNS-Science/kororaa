d776fb6cb8d2dd80b7a25a97822f8f2d148cb26a
Chris DiCaprio <christopher.dicaprio@gmail.com>
2026-03-20 06:29:56 +1300

    Fix/number of ticks (#506)
    * set number of ticks to 2 for log x-axis

--- src/views/hazardCharts/HazardCharts.tsx
+++ src/views/hazardCharts/HazardCharts.tsx
@@ -122,6 +122,7 @@ const HazardCharts: React.FC<HazardChartsProps> = ({ data, state, dispatch }: Ha
               poe={state.poe}
               uncertainty={state.hazardUncertainty}
               timePeriod={state.timePeriod}
+              numTickX={state.hazardXScale === "linear" ? null : 2}
             />
           </div>
         </ChartContainer>
@@ -148,6 +149,7 @@ const HazardCharts: React.FC<HazardChartsProps> = ({ data, state, dispatch }: Ha
                 poe={state.poe}
                 uncertainty={state.spectralUncertainty}
                 timePeriod={state.timePeriod}
+                numTickX={state.spectraXScale === "linear" ? null : 2}
               />
             </div>
           </ChartContainer>
