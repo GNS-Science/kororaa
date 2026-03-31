2ac3f3ba25fc63e89618218d42184b1c5893c634
Chris DiCaprio <christopher.dicaprio@gmail.com>
2026-04-01 12:47:45 +1300

    scientific notation on hazard plot y-axis tick labels (#514)
    scientific notation on hazard plot y-axis tick labels

--- src/views/hazardCharts/HazardCharts.tsx
+++ src/views/hazardCharts/HazardCharts.tsx
@@ -123,6 +123,7 @@ const HazardCharts: React.FC<HazardChartsProps> = ({ data, state, dispatch }: Ha
               uncertainty={state.hazardUncertainty}
               timePeriod={state.timePeriod}
               numTickX={state.hazardXScale === "linear" ? null : 2}
+              yTickFormat={(v) => v.valueOf().toExponential(1)}
             />
           </div>
         </ChartContainer>
